f5fcc552eb6f0de5af299b558fba3ebaacb05f38
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-27 17:41:21 +0530

    template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in HI Apoorv
+                    You are logged in Hi Apoorv
                 </div>
             </div>
         </div>
