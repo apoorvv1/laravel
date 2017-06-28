74fb50255022357b64744eb8fef7a0b9712af986
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-29 00:11:05 +0530

    32

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,111 @@
-@extends('layouts.app')
+<!doctype html>
+<html >
+<head>
 
-@section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <title>RSPInnovations LLP</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+    <link href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+    <!-- Custom Fonts -->
+    <link href="{{ asset('/vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+    <!-- Theme CSS -->
+    <link href="{{ asset('/css/grayscale.min.css')}}" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <i class="fa fa-play-circle"></i> <span class="light">RSP</span> Innovations
+            </a>
+        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                @if (Route::has('login'))
+                    <li>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                        @if (Auth::check())
+                            <a class="page-scroll" href="{{ url('/home') }}">Home</a>
+                    </li>
+                @else
+                    <li>
+                        <a class="page-scroll" href="{{ url('/login') }}">Login</a>
+                    </li>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                @endif
+                @endif
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<header class="intro">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+
+                    <p class="intro-text">Welcome to our World of Innovation.
+                        <br>RSPInnov LLP is one of the pioneering software and Web application development organizations aimed at creating applications that match both technology and creativity with a touch of imagination to create the unimaginable range of products benefiting society and serving everyone in all walks of life in turn a fast transition from present to future.</p>
+                    <a href="#about" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
                 </div>
             </div>
         </div>
-        </div>
+    </div>
+</header>
 
+<!-- About Section -->
+
+<!-- Map Section -->
 <div id="map"></div>
-@endsection
+
+<!-- Footer -->
+<footer>
+    <div class="container text-center">
+        <p>Copyright &copy; 2017 <a href="http://rspinnovations.com/">RSPInnov LLP</a>
+    </div>
+</footer>
+
+<!-- jQuery -->
+
+<script src="{{ asset ('/vendor/jquery/jquery.js') }}"></script>
+
+
+<script src="{{ asset ('/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+
+<!-- Plugin JavaScript -->
+<script src= 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+
+<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+
+<script src= "https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+<!-- Theme JavaScript -->
+<script src="{{ asset ('/js/grayscale.min.js')}}"></script>
+
+</body>
+</html>
