7a151be37fe47f30c44e692a1bc77bbbcba05f25
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-28 23:44:51 +0530

    23

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -107,15 +107,7 @@
                                 <a href="#" class="btn btn-default btn-flat"></a>
                             </div>
                             <div class="pull-right">
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
+                                
                             </div>
                         </li> @endif
                     </ul>
