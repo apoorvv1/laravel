d21eb2f95f3cd005e9a422508a8390403d8f8b32
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-29 00:01:06 +0530

    28

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
+Route::get('/', function () {
     return view('dashboard');
 });
 Auth::routes();
