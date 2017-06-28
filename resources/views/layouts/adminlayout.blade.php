7b06c55608ee4df9a3e6f54c2a1d3574ad379158
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-29 01:06:59 +0530

    41

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -62,32 +62,53 @@
 
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
+                <!-- Messages: style can be found in dropdown.less-->
+
+                <!-- Notifications: style can be found in dropdown.less -->
+
+                <!-- Tasks: style can be found in dropdown.less -->
+                <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                        @else
-                        <span class="hidden-xs"> {{ Auth::user()->name }}</span>
+                        <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                        <span class="hidden-xs">Apoorv verma</span>
                     </a>
                     <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
+                            <p>
+                                Apoorv verma - Web Developer
+
+                            </p>
+                        </li>
+                        <!-- Menu Body -->
+                        <li class="user-body">
+                            <div class="row">
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Followers</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Sales</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Friends</a>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                        </li>
+                        <!-- Menu Footer-->
                         <li class="user-footer">
-
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat"></a>
+                            </div>
                             <div class="pull-right">
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();" class="btn btn-default btn-flat">Logout</a><form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-
+                                <a href="signout.php" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
                 </li>
-
-                         @endif
-
-                
                 <!-- Control Sidebar Toggle Button -->
 
             </ul>
