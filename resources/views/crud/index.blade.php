97cd78570951ceabfcaa2c9de7d3d4db3261dac5
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-06-30 16:55:26 +0530

    70

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -10,7 +10,7 @@
 
                 <div class="box">
                     <div class="box-header">
-                        <h3 class="box-title">CRUD operations with Ajax</h3>
+                        <h3 class="box-title" align="center"><b>CRUD operations with Ajax</b></h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
